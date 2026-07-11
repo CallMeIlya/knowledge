1c7416045413c4ed6ab26c54af244ce690d4958d
Ilya <lee.kuznetsov@gmail.com>
2026-07-11 13:45:32 +0200

    lol

--- neatscripts/charscript.py
+++ neatscripts/charscript.py
@@ -1,18 +1,19 @@
 with open("file.txt") as r, open("final.txt", 'a') as w:
 
     for line in r:
+        arr = arr.replace('\t', ' ')
         arr = line.split(" ")
 
         #print(arr[0].split('\t'))
         
-        end = ""
+        end = "## "+arr[0]('\t',' ')+"\n"
+        #end=end+arr[1].replace('\t', '')
+
         for i in range(1,len(arr)):
-            end=end+" "+arr[i].replace('\t', ' ')
-        #print(end)
-        #print(end.encode().hex())
-        #print(arr[0].split('\t'))
-        #print(arr)
-        addedLine = f"## {arr[0].split('\t')[0]}\n{arr[0].split('\t')[1]} {end}"
+            end=end+" "+arr[i]
+        
+        #addedLine = f"## {arr[0].split('\t')[0]}\n{arr[0].split('\t')[1]} {end}"
+        addedLine=end
+        #addedLine.replace('\t', ' ')
         w.write(addedLine)
-        #print(arr)
 
